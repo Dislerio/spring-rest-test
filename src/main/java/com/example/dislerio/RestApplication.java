966ac53dc91841b74f831f1c64f66b9728bca753
poryadin.vasiliy <poryadin@gmail.com>
2020-02-02 15:01:23 +0300

    3st commit + add log4j2 logging to controller

--- src/main/java/com/example/dislerio/RestApplication.java
+++ src/main/java/com/example/dislerio/RestApplication.java
@@ -1,5 +1,7 @@
 package com.example.dislerio;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,8 +9,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class RestApplication {
+	private static final Logger logger = LogManager.getLogger(RestApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
 	}
+
 }
--- src/main/java/com/example/dislerio/controller/ProductController.java
+++ src/main/java/com/example/dislerio/controller/ProductController.java
@@ -3,13 +3,15 @@ package com.example.dislerio.controller;
 import com.example.dislerio.exception.ResourceNotFoundException;
 import com.example.dislerio.model.Product;
 import com.example.dislerio.repository.ProductsRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,21 +19,27 @@ import java.util.stream.Collectors;
 @RequestMapping("/api")
 public class ProductController {
 
+    private static final Logger logger = LogManager.getLogger(ProductController.class);
+
     @Autowired
     ProductsRepository productsRepository;
 
     @GetMapping("/products")
     public List<Product> getTailings() {
+        logger.info(getUserNameWithDate() + "getting Tailings");
         return productsRepository.findAll().stream().filter(p -> p.getQuantity() < 5).collect(Collectors.toList());
     }
 
     @PostMapping("/products")
     public Product createProduct(@Valid @RequestBody Product product) {
+
+        logger.info(getUserNameWithDate() + "creating a new Product");
         return productsRepository.save(product);
     }
 
     @GetMapping("/products/{id}")
     public Product getProductById(@PathVariable(value = "id") Long productId) {
+        logger.info(getUserNameWithDate() + "getting info about Product with ID: " + productId);
         return productsRepository.findById(productId)
                 .orElseThrow(() -> new ResourceNotFoundException("Product", "id", productId));
     }
@@ -45,18 +53,19 @@ public class ProductController {
 
         product.setTitle(productDetails.getTitle());
         product.setBrand(productDetails.getBrand());
-
-        Product updatedProduct = productsRepository.save(product);
-        return updatedProduct;
+        logger.warn(getUserNameWithDate() + "updating info about Product with ID: " + productId);
+        return productsRepository.save(product);
     }
 
     @DeleteMapping("/products/{id}")
     public ResponseEntity<?> deleteProduct(@PathVariable(value = "id") Long productId) {
         Product product = productsRepository.findById(productId)
                 .orElseThrow(() -> new ResourceNotFoundException("Product", "id", productId));
-
         productsRepository.delete(product);
-
+        logger.debug(getUserNameWithDate() + "deleting Product with ID: " + productId);
         return ResponseEntity.ok().build();
+    }
+    private String getUserNameWithDate(){
+        return SecurityContextHolder.getContext().getAuthentication().getName() + ": " + new Date() + " - ";
     }
 }
