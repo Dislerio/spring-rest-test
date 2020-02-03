c689735451fb7ab0de3663d2cf94e7112643bbe9
poryadin.vasiliy <poryadin@gmail.com>
2020-02-03 11:59:48 +0300

    final + testing + yml settings + war

--- src/main/java/com/example/dislerio/RestApplication.java
+++ src/main/java/com/example/dislerio/RestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+//@EnableJpaAuditing
 public class RestApplication {
 	private static final Logger logger = LogManager.getLogger(RestApplication.class);
 
--- src/test/java/com/example/easynotes/RestApplicationTests.java
+++ src/test/java/com/example/easynotes/RestApplicationTests.java
@@ -2,11 +2,12 @@ package com.example.easynotes;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = RestApplicationTests.class)
 public class RestApplicationTests {
 
 	@Test
